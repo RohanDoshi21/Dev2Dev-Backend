019ba1e6b7abb4e5b3e403304cdda9b54cb08ba1
Rohan Doshi <rohan.doshi02@gmail.com>
2023-02-20 18:05:40 +0530

    Display questions with conditional rendering for status

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
+// import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import TopQuestions from "./components/TopQuestions";
 
 function App() {
-  return (
-    <Header/>
-  );
+	return (
+		<>
+			<Header />
+			<TopQuestions />
+		</>
+	);
 }
 
 export default App;
