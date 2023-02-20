73cc88e26642e4018253ef80f1fd4f31a05be6c3
Rohan Doshi <rohan.doshi02@gmail.com>
2023-02-20 18:00:34 +0530

    Cors policy changes

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ let express = require("express");
 let path = require("path");
 let cookieParser = require("cookie-parser");
 let logger = require("morgan");
+let cors = require("cors");
 
 let indexRouter = require("./routes/index");
 let authRouter = require("./routes/auth");
@@ -11,6 +12,7 @@ let tagRouter = require("./routes/tag");
 
 let app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
