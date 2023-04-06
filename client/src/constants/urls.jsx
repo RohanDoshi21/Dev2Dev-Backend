38e585384c9bd7e7a4a9a86fb3817a529d2301b3
Rohan Doshi <rohan.doshi02@gmail.com>
2023-04-07 02:28:09 +0530

    Unnecessary files removed (#12)

--- client/src/constants/urls.jsx
+++ client/src/constants/urls.jsx
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:8000";
+export const baseUrl = process.env.BACKEND_BASE_URL;
 export const getQuestionsUrl = baseUrl + "/questions";
 export const signupUrl = baseUrl + "/auth/signup";
 export const loginUrl = baseUrl + "/auth/login";
